416fc6560a20e104ba703e9dbb41c01e2e0a7259
quinlan-alys <shannonqad@gmail.com>
2016-11-14 16:29:55 -0800

    new classes for mars colony

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Does app really work tho?';
+ message = 'This is a paragraph under title.';
+  div = 'also working for divs!';
+  
+}
