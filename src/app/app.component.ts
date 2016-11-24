9547b1acd56ab5702467971e70be3005d589176d
quinlan-alys <shannonqad@gmail.com>
2016-11-24 09:50:36 -0800

    organizing sass and adjusting heights

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,10 @@ import {ViewEncapsulation} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['app.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'Does app really work tho?';
- message = 'This is a paragraph under title.';
-  div = 'also working for divs!';
+
   
 }
